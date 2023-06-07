1ce7dfc0631ce2d685000360fb29a93d0342adc1
Almazov Urmat <almazov.urmat@gmail.com>
2023-06-07 18:13:12 +0600

    Styles and contact examples have been added

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -9,6 +9,34 @@ const Users: React.FC <IProps> = ({users}) => {
     return (
         <>
             <h4>List of users</h4>
+            <div className="user">
+                <div className="card mb-2">
+                    <div className="row no-gutters">
+                        <div className="col-sm-12">
+                            <div className="card-body">
+                                <h6 className="card-title">Name: Admin <span className="status available"></span></h6>
+                                <p className="card-text small">Email: superadmin@mail.com</p>
+                                <p className="card-text">Role: Admin</p>
+                                <p className="card-text">Status: Active</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="user">
+                <div className="card mb-2">
+                    <div className="row no-gutters">
+                        <div className="col-sm-12">
+                            <div className="card-body">
+                                <h6 className="card-title">Name: User <span className="status unavailable"></span></h6>
+                                <p className="card-text small">Email: blockeduser@mail.com</p>
+                                <p className="card-text">Role: User</p>
+                                <p className="card-text">Status: Inactive</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             {
                 users.map((user: IUser) => {
                     return <UserItem key={user.id} item={user} />

a3269d3f1116f0ed1dda4fd3d22e31820c2aace1
Almazov Urmat <almazov.urmat@gmail.com>
2023-06-07 17:07:35 +0600

    added functionality for UserItem.tsx

--- src/components/Users/UserItem.tsx
+++ src/components/Users/UserItem.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import {IUser} from "../../types";
 
-const UserItem = () => {
+interface IProps {
+    item: IUser
+}
+
+const UserItem: React.FC<IProps> = ({item}) => {
+    const cssClassName = `status ${item.active ? 'available' : 'unavailable'}`;
+
     return (
-        <div>
-            
+        <div className="user">
+            <div className="card mb-2">
+                <div className="row no-gutters">
+                    <div className="col-sm-12">
+                        <div className="card-body">
+                            <h6 className="card-title">Name: {item.name} <span className={cssClassName}></span></h6>
+                            <p className="card-text small">Email: {item.email}</p>
+                            <p className="card-text">Role: {item.role}</p>
+                            <p className="card-text">Status: {item.active ? 'Active' : 'Inactive'}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
